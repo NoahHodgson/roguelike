02ce63c3c7736d510a2bcd895359e9dfacf98879
Noah <noahmhodgson@gmail.com>
2022-01-07 12:32:01 -0500

    added fov

--- engine.py
+++ engine.py
@@ -6,6 +6,7 @@ from tcod.console import Console
 from game_map import GameMap
 from entity import Entity
 from input_handlers import EventHandler
+from tcod.map import compute_fov
 
 
 class Engine:
@@ -14,7 +15,8 @@ class Engine:
         self.event_handler = event_handler
         self.player = player
         self.game_map = game_map
-        
+        self.update_fov()
+
 
     def handle_events(self, events: Iterable[Any]) -> None:
         for event in events:
@@ -25,11 +27,24 @@ class Engine:
 
             action.perform(self, self.player)
 
+            self.update_fov()  # Update the FOV before the players next action.
+    
+    def update_fov(self) -> None:
+        """Recompute the visible area based on the players point of view."""
+        self.game_map.visible[:] = compute_fov(
+            self.game_map.tiles["transparent"],
+            (self.player.x, self.player.y),
+            radius=8,
+        )
+        # If a tile is "visible" it should be added to "explored".
+        self.game_map.explored |= self.game_map.visible
 
     def render(self, console: Console, context: Context) -> None:
         self.game_map.render(console)
         for entity in self.entities:
-            console.print(entity.x, entity.y, entity.char, fg=entity.color)
+            # Only print entities that are in the FOV
+            if self.game_map.visible[entity.x, entity.y]:
+                console.print(entity.x, entity.y, entity.char, fg=entity.color)
 
         context.present(console)
 

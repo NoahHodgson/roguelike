be331b2bec23cacd6b00d7fd62b5aebfba892ec6
Noah <noahmhodgson@gmail.com>
2022-01-09 17:58:55 -0500

    enemy ai added, health bar added, game over added. starting to look like a game

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 import tcod
+import color
+import copy
 from engine import Engine
-from entity import Entity
-from input_handlers import EventHandler
+import entity_maker
 from game_map import GameMap
 from procgen import generate_dungeon
 
@@ -12,31 +13,33 @@ def main() -> None:
     screen_height = 50
 
     map_width = 80
-    map_height = 45
+    map_height = 43
 
     room_max_size = 10
     room_min_size = 6
     max_rooms = 30
+    max_monsters_per_room = 3
 
     tileset = tcod.tileset.load_tilesheet(
         "image/tileset.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
+    player = copy.deepcopy(entity_maker.player)
+    engine = Engine(player=player)
+    
+    engine.message_log.add_message(
+        "Hello and welcome, adventurer, to yet another dungeon!", color.welcome_text
+    )
 
-    event_handler = EventHandler()    
-
-    player = Entity(int(screen_width / 2), int(screen_height / 2), "@", (255, 255, 255))
-    npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), "@", (255, 255, 0))
-    entities = {npc, player}
-
-    game_map = generate_dungeon(
+    engine.game_map = generate_dungeon(
         max_rooms=max_rooms,
         room_min_size=room_min_size,
         room_max_size=room_max_size,
         map_width=map_width,
         map_height=map_height,
-        player=player
+        max_monsters_per_room=max_monsters_per_room,
+        engine=engine,
     )    
-    engine = Engine(entities=entities, event_handler=event_handler, game_map=game_map, player=player)    
+    engine.update_fov()   
 
     with tcod.context.new_terminal(
         screen_width,
@@ -47,12 +50,12 @@ def main() -> None:
     ) as context:
         root_console = tcod.Console(screen_width, screen_height, order="F")
         while True:
-            engine.render(console=root_console, context=context)
-
-            events = tcod.event.wait()
-
-            engine.handle_events(events)
-
+            root_console.clear()
+            engine.event_handler.on_render(console=root_console)
+            context.present(root_console)
+            engine.render(console=root_console)
+ 
+            engine.event_handler.handle_events(context)
 
 if __name__ == "__main__":
     main()

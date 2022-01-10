2b9cb1c4630db08b616684ed06a25f247a96e2b5
Noah <noahmhodgson@gmail.com>
2022-01-10 13:37:33 -0500

    more items, aim feature

--- procgen.py
+++ procgen.py
@@ -57,7 +57,15 @@ def place_entities(
         y = random.randint(room.y1 + 1, room.y2 - 1)
 
         if not any(entity.x == x and entity.y == y for entity in dungeon.entities):
-            entity_maker.health_potion.spawn(dungeon, x, y)
+            item_chance = random.random()
+            if item_chance < 0.7:
+                entity_maker.health_potion.spawn(dungeon, x, y)
+            elif item_chance < 0.8:
+                entity_maker.fireball_scroll.spawn(dungeon, x, y)
+            elif item_chance < 0.9:
+                entity_maker.confusion_scroll.spawn(dungeon, x, y)
+            else:
+                entity_maker.lightning_scroll.spawn(dungeon, x, y)
 
 
 def tunnel_between(

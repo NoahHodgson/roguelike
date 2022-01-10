e00c5accb34ecd89c8c308251a0280644bfe7ed1
Noah <noahmhodgson@gmail.com>
2022-01-10 16:25:09 -0500

    added main menu

--- main.py
+++ main.py
@@ -3,69 +3,54 @@ import tcod
 import color
 import copy
 import traceback
-from engine import Engine
-import entity_maker
+import exceptions
+import input_handlers
 from game_map import GameMap
-from procgen import generate_dungeon
+import setup_game
 
 
 def main() -> None:
     screen_width = 80
     screen_height = 50
-
-    map_width = 80
-    map_height = 43
-
-    room_max_size = 10
-    room_min_size = 6
-    max_rooms = 30
-    max_monsters_per_room = 3
-    max_items_per_room = 2
-
     tileset = tcod.tileset.load_tilesheet(
         "image/tileset.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
-    player = copy.deepcopy(entity_maker.player)
-    engine = Engine(player=player)
-    
-    engine.message_log.add_message(
-        "Hello and welcome, adventurer, to yet another dungeon!", color.welcome_text
-    )
 
-    engine.game_map = generate_dungeon(
-        max_rooms=max_rooms,
-        room_min_size=room_min_size,
-        room_max_size=room_max_size,
-        map_width=map_width,
-        map_height=map_height,
-        max_monsters_per_room=max_monsters_per_room,
-        max_items_per_room=max_items_per_room,
-        engine=engine,
-    )    
-    engine.update_fov()   
+    handler: input_handlers.BaseEventHandler = setup_game.MainMenu()
 
     with tcod.context.new_terminal(
         screen_width,
         screen_height,
         tileset=tileset,
-        title="Yet Another Roguelike Tutorial",
+        title="Twilight Spirit Trinity",
         vsync=True,
     ) as context:
         root_console = tcod.Console(screen_width, screen_height, order="F")
-        while True:
-            root_console.clear()
-            engine.event_handler.on_render(console=root_console)
-            context.present(root_console)
-            engine.render(console=root_console)
- 
-            try:
-                for event in tcod.event.wait():
-                    context.convert_event(event)
-                    engine.event_handler.handle_events(event)
-            except Exception:  # Handle exceptions in game.
-                traceback.print_exc()  # Print error to stderr.
-                # Then print the error to the message log.
-                engine.message_log.add_message(traceback.format_exc(), color.error)
+        try:
+            while True:
+                root_console.clear()
+                handler.on_render(console=root_console)
+                context.present(root_console)
+
+                try:
+                    for event in tcod.event.wait():
+                        context.convert_event(event)
+                        handler = handler.handle_events(event)
+                except Exception:  # Handle exceptions in game.
+                    traceback.print_exc()  # Print error to stderr.
+                    # Then print the error to the message log.
+                    if isinstance(handler, input_handlers.EventHandler):
+                        handler.engine.message_log.add_message(
+                            traceback.format_exc(), color.error
+                        )
+        except exceptions.QuitWithoutSaving:
+            raise
+        except SystemExit:  # Save and quit.
+            # TODO: Add the save function here
+            raise
+        except BaseException:  # Save on any other unexpected exception.
+            # TODO: Add the save function here
+            raise
 
 if __name__ == "__main__":
     main()
